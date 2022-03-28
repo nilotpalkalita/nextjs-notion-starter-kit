6c2b8ef57ecc92fb509bc37a2759369c84193c52
Nilotpal Kalita <nilotpalkalita@hotmail.com>
2022-03-29 03:57:46 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 export default {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'fd0100c39dcf4ffd88a6a7ad7fe6fd74',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
